1b8c2026f7855542711f79ceced8019d087e45a2
Fabien <fab0670312047@gmail.com>
2022-11-11 22:40:50 +0100

    [PERFORMANCE] Optimise performance and make sure the style look good

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,5 @@
 class HomeController < ApplicationController
   def index
-    @image_requests = ImageRequest.all
+    @image_requests = ImageRequest.includes(:synced_generated_images).all
   end
 end
