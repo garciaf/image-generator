fbed50636ddff02eb9edc24efbe5cb11aa68807f
Garcia Fabien <fab0670312047@gmail.com>
2022-11-16 10:44:54 +0100

    [CLEANUP] remove dead code

--- app/models/image_request.rb
+++ app/models/image_request.rb
@@ -4,6 +4,8 @@ class ImageRequest < ApplicationRecord
   validates :prompt, :size, presence: true
   attribute :size, :string, default: '1024x1024'
 
+  broadcasts
+
   def retrieve_image!
     store_image retrieve_image_url_from_api
   end
@@ -11,7 +13,6 @@ class ImageRequest < ApplicationRecord
   private
 
   def store_image(generated_image_url)
-    uri = URI.parse(generated_image_url)
     tempfile = Down.download(generated_image_url)
     images.attach(io: tempfile, filename: tempfile.original_filename, content_type: tempfile.content_type)
   end
